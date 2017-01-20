dd765004f3a2a24ae691aba6d17c90db43166174
Elias Naur <elias.naur@gmail.com>
2017-01-20 14:06:41 +0100

    wip

--- src/gorev/gorev.go
+++ src/gorev/gorev.go
@@ -4,6 +4,11 @@ import (
 	"Java/android/os"
 	"Java/android/support/v7/app"
 
+	"Java/android/databinding/DataBindingUtil"
+	"Java/gorev/databinding/ActivityMainBinding"
+
+	rlayout "Java/gorev/R/layout"
+
 	// The Java package for "gorev".
 	gopkg "Java/gorev"
 )
@@ -14,4 +19,7 @@ type GoActivity struct {
 
 func (a *GoActivity) OnCreate(this gopkg.GoActivity, b os.Bundle) {
 	this.Super().OnCreate(b)
+	db := DataBindingUtil.SetContentView(this, rlayout.Activity_main)
+	binding := ActivityMainBinding.Cast(db)
+	binding.SetText("Hello from the reverse")
 }
